e3d9c72da22b6ad6f5ca16d21bb09e74ec92453f
Kimsungil <kittoboy@naver.com>
2016-07-12 17:20:39 +0900

    TextView 글자 크기 변경 => 12.5f

--- app/src/main/java/com/dev_daddy/helloandroid/MainActivity.java
+++ app/src/main/java/com/dev_daddy/helloandroid/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTvHello = (TextView) findViewById(R.id.tv_hello);
+        mTvHello.setTextSize(12.5f);
     }
 }
